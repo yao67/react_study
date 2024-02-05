6e26d581ce36da892086308ca6e4544d754895b0
JaeGyeong, Jo <jaegj96@gmail.com>
2024-02-05 11:44:09 +0900

    create-react-app 기본 프로젝트에서 typescript 개발 설정

--- react-tutorial/src/index.tsx
+++ react-tutorial/src/index.tsx
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('root 요소를 찾을 수 없음');
+
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
